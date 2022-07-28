aaa225217e731e04f75fa9e8c811e797695bb51c
roux g. buciu <roux@mozilla.com>
2022-07-28 17:00:49 -0400

    Add a lot of stuff and project 2

---
+++ HackingWithSwift/100DaysSwiftUI/Project2/Guess the Flag/Guess the Flag/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Guess the Flag
+//
+//  Created by Roux Buciu on 2022-07-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
