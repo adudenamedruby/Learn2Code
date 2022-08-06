2406f7a2cae490b001900058b81c0eb619dca324
roux g. buciu <roux@mozilla.com>
2022-08-05 22:23:59 -0400

    Changes

---
+++ RayWenderlich/TDD/03-tdd-app-setup/projects/starter/FitNess/FitNessTests/StepCountControllerTests.swift
@@ -0,0 +1,12 @@
+//
+// Created by Roux Buciu on 2022-08-05.
+// Copyright (c) 2022 Ray Wenderlich. All rights reserved.
+//
+
+import XCTest
+
+@testable import FitNess
+
+class StepCountControllerTests: XCTestCase {
+    
+}
