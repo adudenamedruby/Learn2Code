2130551f759bd69906e667fdfa1e0eb415aea7fa
roux g. buciu <roux@mozilla.com>
2023-06-13 09:20:41 -0400

    Update learning repo

--- Languages/Swift/theSandbox.playground/Contents.swift
+++ Languages/Swift/theSandbox.playground/Contents.swift
@@ -1,2 +1,13 @@
 import Cocoa
 
+struct Tho {
+    let enabled: Bool
+}
+
+var test = ["first": Tho(enabled: true), "second": Tho(enabled: false)]
+
+var loofah = test.filter { key, value in
+    value.enabled
+}
+
+print(loofah)
