7aa5fbe7d44f529421e56917638ef5322f80a83a
roux g. buciu <github@fringe.foundation>
2023-08-01 09:02:39 -0400

    changes from theLab on Tue 1 Aug 2023 09:02:38 EDT

--- Languages/Swift/theSandbox.playground/Contents.swift
+++ Languages/Swift/theSandbox.playground/Contents.swift
@@ -1,3 +1,19 @@
 import Cocoa
-import UIKit
 
+class ShoppingProduct {
+    var name: String
+    
+    var bingBong: Bool {
+        return name.isEmpty
+    }
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+let string: URL? = URL(string: "https://macrumors.com")
+let product = string.flatMap(ShoppingProduct.init)
+let boolean = product?.bingBong
+
+print(boolean)
