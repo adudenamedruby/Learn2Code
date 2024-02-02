6b08ffe2b602fe37ab9e9f2025e4e482be321c13
roux g. buciu <github@adudenamedruby.com>
2024-02-01 20:32:27 -0500

    changes from theLab on Thu 1 Feb 2024 20:32:26 EST

--- Languages/Swift/theSandbox.playground/Contents.swift
+++ Languages/Swift/theSandbox.playground/Contents.swift
@@ -1,19 +1,23 @@
-import Cocoa
-
-class ShoppingProduct {
-    var name: String
-    
-    var bingBong: Bool {
-        return name.isEmpty
-    }
-    
-    init(name: String) {
-        self.name = name
-    }
+struct Engine {
+    var engineID: String?
 }
 
-let string: URL? = URL(string: "https://macrumors.com")
-let product = string.flatMap(ShoppingProduct.init)
-let boolean = product?.bingBong
+enum Locat: String {
+    case first
+}
 
-print(boolean)
+let amazon = Engine(engineID: "amazondotcom")
+let custom = Engine(engineID: nil)
+
+let extra = [
+    "locat": Locat.first,
+    "engine": custom.engineID as Any
+] as [String: Any]
+
+if let locat = extra["locat"] as? Locat,
+   let engine = extra["engine"] as? String? {
+    print(locat.rawValue)
+    print(engine ?? "customisz")
+}
+
+print(custom.engineID ?? "customizeeee")
